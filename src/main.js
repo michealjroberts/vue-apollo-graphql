de2dfa8ef498c96572a87a4c50316e84781a7aa3
Michael Roberts <michael@winduplordvexxos.com>
2018-09-18 21:16:16 +0100

    Initial commit of Vue + Apollo + GraphQL project
    Initial commit of the base Vue + Apollo Engine + GraphQL project for vuejs.london (talk by Sara Vieira)

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import "./registerServiceWorker";
+import { createProvider } from "./vue-apollo";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  apolloProvider: createProvider(),
+  render: h => h(App)
+}).$mount("#app");
